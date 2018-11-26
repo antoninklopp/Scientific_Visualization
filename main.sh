7bbc04c39f973edcf11c9ed758aee4caa3e0c3d4
Antonin KLOPP-TOSSER <antonin.klopp-tosser@grenoble-inp.org>
2018-11-26 16:10:40 +0100

    Ajout du script pour la carte isovaleur

--- main.sh
+++ main.sh
@@ -1,20 +1,41 @@
 #!/bin/sh
 
 #Enlever tous les anciens fichiers image
-# rm images/*.png > /dev/null
+rm images/*.png > /dev/null 2>&1
+
+for i in "$@"
+do
+case $i in
+    -h|--help)
+    echo "Visualisation scientifique
+
+Lancer ./main.sh argument
+
+--default (ou sans argument) : visualisation de la
+carte avec iso valurs, lignes de courant et temperature
+
+--iso : visualisation des lignes iso valeurs de temperature"
+    exit
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
 
 # Parametre 1 : Type de format (ex : SP1)
 for i in {1..7}
 do
-    rm *.grib2 > /dev/null
+    rm *.grib2 > /dev/null 2>&1
 
     # Enlever les anciens output files
-    rm *.nc > /dev/null
+    rm *.nc > /dev/null 2>&1
 
-    echo $((($i-1)*6 - 5))
-    python3 RequeteArome.py $((($i-1)*6 - 5)) $1 # intervalle de 6 heures entre chaque données.
+    echo "Téléchargement carte $i"
+    python3 RequeteArome.py $((($i-1)*6 - 5)) $1 > /dev/null 2>&1 # intervalle de 6 heures entre chaque données.
 
-    NOM_FICHIER="$(ls *.grib2 | head -n1)"
+    NOM_FICHIER="$(ls *.grib2 | head -n1)" > /dev/null 2>&1
 
     while [ -z "$NOM_FICHIER" ]
     do
@@ -23,23 +44,35 @@ do
 
       sleep 30
 
-      rm *.grib2 > /dev/null
+      rm *.grib2 > /dev/null 2>&1
 
       # Enlever les anciens output files
-      rm *.nc > /dev/null
+      rm *.nc > /dev/null 2>&1
 
       echo $((($i-1)*6 - 5))
-      python3 RequeteArome.py $((($i-1)*6 - 5)) $1 # intervalle de 6 heures entre chaque données.
+      python3 RequeteArome.py $((($i-1)*6 - 5)) $1 > /dev/null 2>&1 # intervalle de 6 heures entre chaque données.
 
       NOM_FICHIER="$(ls *.grib2 | head -n1)"
 
   done
 
     # Convertir fichier en grib2
-    ./wgrib2 $NOM_FICHIER -netcdf output.nc > /dev/null
+    ./wgrib2 $NOM_FICHIER -netcdf output.nc > /dev/null 2>&1
 
     # Process avec paraview
-    pvpython process_data.py output.nc $i > /dev/null
+    for i in "$@"
+    do
+    case $i in
+        --iso)
+        pvpython process_data_iso.py output.nc $i > /dev/null 2>&1
+        shift 
+        ;;
+        *)
+        pvpython process_data.py output.nc $i > /dev/null 2>&1
+        ;;
+    esac
+    done
+
 
 done
 
--- process_data_iso.py
+++ process_data_iso.py
@@ -19,7 +19,7 @@ renderView1.CenterOfRotation = [1.99999999998727, 46.45, 0.0]
 renderView1.StereoType = 0
 renderView1.CameraPosition = [2.16261017317024, 46.3371612595104, 64.2005781312117]
 renderView1.CameraFocalPoint = [2.16261017317024, 46.3371612595104, 0.0]
-renderView1.CameraParallelScale = 11.281891230701357
+renderView1.CameraParallelScale = 11.281891230701355
 
 # ----------------------------------------------------------------
 # setup the data processing pipelines
@@ -35,6 +35,36 @@ meteonc.ReplaceFillValueWithNan = 1
 calculator1 = Calculator(Input=meteonc)
 calculator1.Function = 'iHat*UGRD_10maboveground+jHat*VGRD_10maboveground'
 
+# create a new 'Contour'
+contour3 = Contour(Input=calculator1)
+contour3.ContourBy = ['POINTS', 'TMP_2maboveground']
+contour3.Isosurfaces = [295.0]
+contour3.PointMergeMethod = 'Uniform Binning'
+
+# create a new 'Contour'
+contour6 = Contour(Input=calculator1)
+contour6.ContourBy = ['POINTS', 'TMP_2maboveground']
+contour6.Isosurfaces = [270.0]
+contour6.PointMergeMethod = 'Uniform Binning'
+
+# create a new 'Contour'
+contour7 = Contour(Input=calculator1)
+contour7.ContourBy = ['POINTS', 'TMP_2maboveground']
+contour7.Isosurfaces = [265.0]
+contour7.PointMergeMethod = 'Uniform Binning'
+
+# create a new 'Contour'
+contour2 = Contour(Input=calculator1)
+contour2.ContourBy = ['POINTS', 'TMP_2maboveground']
+contour2.Isosurfaces = [290.0]
+contour2.PointMergeMethod = 'Uniform Binning'
+
+# create a new 'Contour'
+contour1 = Contour(Input=calculator1)
+contour1.ContourBy = ['POINTS', 'TMP_2maboveground']
+contour1.Isosurfaces = [285.0]
+contour1.PointMergeMethod = 'Uniform Binning'
+
 # create a new 'Glyph'
 glyph1 = Glyph(Input=calculator1,
     GlyphType='Arrow')
@@ -44,20 +74,16 @@ glyph1.ScaleFactor = 0.3
 glyph1.GlyphTransform = 'Transform2'
 
 # create a new 'Contour'
-contour1 = Contour(Input=calculator1)
-contour1.ContourBy = ['POINTS', 'TMP_2maboveground']
-contour1.Isosurfaces = [285.0]
-contour1.PointMergeMethod = 'Uniform Binning'
+contour5 = Contour(Input=calculator1)
+contour5.ContourBy = ['POINTS', 'TMP_2maboveground']
+contour5.Isosurfaces = [275.0]
+contour5.PointMergeMethod = 'Uniform Binning'
 
-# create a new 'Stream Tracer'
-streamTracer1 = StreamTracer(Input=calculator1,
-    SeedType='High Resolution Line Source')
-streamTracer1.Vectors = ['POINTS', 'Result']
-streamTracer1.MaximumStreamlineLength = 27.9999999999745
-
-# init the 'High Resolution Line Source' selected for 'SeedType'
-streamTracer1.SeedType.Point1 = [-5.14258076567153, 38.2186814918074, 0.0]
-streamTracer1.SeedType.Point2 = [5.90851405251298, 55.4898351864761, 0.0]
+# create a new 'Contour'
+contour4 = Contour(Input=calculator1)
+contour4.ContourBy = ['POINTS', 'TMP_2maboveground']
+contour4.Isosurfaces = [280.0]
+contour4.PointMergeMethod = 'Uniform Binning'
 
 # ----------------------------------------------------------------
 # setup color maps and opacity mapes used in the visualization
@@ -66,7 +92,7 @@ streamTracer1.SeedType.Point2 = [5.90851405251298, 55.4898351864761, 0.0]
 
 # get color transfer function/color map for 'TMP2maboveground'
 tMP2mabovegroundLUT = GetColorTransferFunction('TMP2maboveground')
-tMP2mabovegroundLUT.RGBPoints = [258.464172363281, 1.0, 0.0, 1.0, 271.35830954042706, 0.666666666666667, 0.0, 1.0, 273.58996582031205, 0.0, 0.0, 0.498039215686275, 276.3175806474781, 0.0, 0.396078431372549, 0.886274509803922, 278.4252624511721, 0.43921568627451, 0.803921568627451, 1.0, 278.5492553710941, 0.36078431372549, 1.0, 0.756862745098039, 280.7809448242191, 0.0, 1.0, 0.0, 285.86419677734415, 1.0, 1.0, 0.0, 289.2117004394532, 1.0, 0.666666666666667, 0.0, 294.04699707031216, 1.0, 0.0, 0.0, 299.37823486328125, 0.666666666666667, 0.0, 0.0]
+tMP2mabovegroundLUT.RGBPoints = [258.464172363281, 1.0, 1.0, 1.0, 299.37823486328125, 1.0, 1.0, 1.0]
 tMP2mabovegroundLUT.NanColor = [1.0, 1.0, 1.0]
 tMP2mabovegroundLUT.ScalarRangeInitialized = 1.0
 
@@ -74,17 +100,6 @@ tMP2mabovegroundLUT.ScalarRangeInitialized = 1.0
 tMP2mabovegroundPWF = GetOpacityTransferFunction('TMP2maboveground')
 tMP2mabovegroundPWF.Points = [258.464172363281, 1.0, 0.5, 0.0, 299.37823486328125, 1.0, 0.5, 0.0]
 tMP2mabovegroundPWF.ScalarRangeInitialized = 1
-
-# get color transfer function/color map for 'RH2maboveground'
-rH2mabovegroundLUT = GetColorTransferFunction('RH2maboveground')
-rH2mabovegroundLUT.RGBPoints = [17.9334888458252, 0.231373, 0.298039, 0.752941, 59.4647378921509, 0.865003, 0.865003, 0.865003, 100.995986938477, 0.705882, 0.0156863, 0.14902]
-rH2mabovegroundLUT.NanColor = [1.0, 1.0, 1.0]
-rH2mabovegroundLUT.ScalarRangeInitialized = 1.0
-
-# get opacity transfer function/opacity map for 'RH2maboveground'
-rH2mabovegroundPWF = GetOpacityTransferFunction('RH2maboveground')
-rH2mabovegroundPWF.Points = [17.9334888458252, 0.0, 0.5, 0.0, 100.995986938477, 1.0, 0.5, 0.0]
-rH2mabovegroundPWF.ScalarRangeInitialized = 1
 
 # ----------------------------------------------------------------
 # setup the visualization in view 'renderView1'
@@ -102,17 +117,57 @@ meteoncDisplay.ScalarOpacityUnitDistance = 0.194190573529865
 contour1Display = Show(contour1, renderView1)
 # trace defaults for the display properties.
 contour1Display.Representation = 'Points'
-contour1Display.AmbientColor = [0.0, 0.0, 0.0]
+contour1Display.AmbientColor = [1.0, 1.0, 0.0]
 contour1Display.ColorArrayName = [None, '']
 
-# show data from streamTracer1
-streamTracer1Display = Show(streamTracer1, renderView1)
+# show data from contour2
+contour2Display = Show(contour2, renderView1)
 # trace defaults for the display properties.
-streamTracer1Display.Representation = 'Surface With Edges'
-streamTracer1Display.ColorArrayName = ['POINTS', 'RH_2maboveground']
-streamTracer1Display.LookupTable = rH2mabovegroundLUT
-streamTracer1Display.EdgeColor = [1.0, 1.0, 1.0]
+contour2Display.ColorArrayName = [None, '']
+contour2Display.DiffuseColor = [1.0, 0.3333333333333333, 0.0]
+
+# show data from contour3
+contour3Display = Show(contour3, renderView1)
+# trace defaults for the display properties.
+contour3Display.ColorArrayName = [None, '']
+contour3Display.DiffuseColor = [1.0, 0.0, 0.0]
+
+# show data from contour4
+contour4Display = Show(contour4, renderView1)
+# trace defaults for the display properties.
+contour4Display.ColorArrayName = [None, '']
+contour4Display.DiffuseColor = [0.0, 1.0, 0.0]
+
+# show data from contour5
+contour5Display = Show(contour5, renderView1)
+# trace defaults for the display properties.
+contour5Display.ColorArrayName = [None, '']
+contour5Display.DiffuseColor = [0.0, 0.6666666666666666, 1.0]
+
+# show data from contour6
+contour6Display = Show(contour6, renderView1)
+# trace defaults for the display properties.
+contour6Display.ColorArrayName = [None, '']
+contour6Display.DiffuseColor = [0.0, 0.0, 1.0]
+
+# show data from contour7
+contour7Display = Show(contour7, renderView1)
+# trace defaults for the display properties.
+contour7Display.ColorArrayName = [None, '']
+contour7Display.DiffuseColor = [0.6666666666666666, 0.0, 1.0]
 
 output = "images/" + sys.argv[2] + ".png"
 print("fichier sortie", output)
 WriteImage(output)
+
+import cv2
+
+img = cv2.imread("images/" + sys.argv[2] + ".png", -1)
+for i in range(img.shape[0]):
+    for j in range(img.shape[1]):
+        if sum(img[i, j]) > 250*4:
+            img[i, j] = [0, 0, 0, 0]
+
+print("outputing")
+
+cv2.imwrite("images/" + sys.argv[2] + ".png", img)
